dbb9aac6ffa09f598b0268af3a8a2d27c1545683
feysalabdi51 <pheysalupdyy@gmail.com>
2025-07-26 15:25:34 +0300

    updated

---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const getMinutesToRead = (minutes) => {
+    if (minutes < 30) {
+      const cups = Math.ceil(minutes / 5);
+      return "☕️".repeat(cups) + ` ${minutes} min read`;
+    } else {
+      const boxes = Math.ceil(minutes / 10);
+      return "🍱".repeat(boxes) + ` ${minutes} min read`;
+    }
+  };
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date} • {getMinutesToRead(minutes)}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
